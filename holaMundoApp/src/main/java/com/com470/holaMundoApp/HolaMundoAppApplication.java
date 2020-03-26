73b89d286d78a269b18fb3d6616e2f3d28cdddf9
pablonoya <pablonoya18@live.com>
2020-03-26 17:31:50 -0400

    P1 Hola Mundo

---
+++ holaMundoApp/src/main/java/com/com470/holaMundoApp/HolaMundoAppApplication.java
@@ -0,0 +1,13 @@
+package com.com470.holaMundoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoAppApplication.class, args);
+	}
+
+}
